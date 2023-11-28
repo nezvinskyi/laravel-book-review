9771eb8289bd0fc9e1473948e79f798d2781073e
Dmitry Nezvinskyi <nezvinskyi@abss.at>
2023-11-28 10:30:09 +0100

    Popular and Highest rated - the logic

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,9 +14,22 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input("title");
+        $filter = $request->input("filter", '');
 
-        $books = Book::when($title, fn( $query, $title) => $query->title($title)
-        )->get();
+        $books = Book::when(
+            $title,
+            fn($query, $title) => $query->title($title)
+        );
+        
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6month' => $books->popularLastSixMonths(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6month' => $books->highestRatedLastSixMonths(),
+            default => $books->latest()
+        };
+
+        $books = $books->get();
 
         // return view("book.inex", compact('books'));
         return view("books.index", ['books' => $books]);
